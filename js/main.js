8943edceb2ffb9411ba6c8ee2d9d05ca0448e7da
Kasinerava <65457233+Kasinerava@users.noreply.github.com>
2020-10-04 00:06:50 +0300

    АБВГ

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// Активное состояние карты
+// Включаем активное состояние карты
 document.querySelector(`.map`).classList.remove(`map--faded`);
 
-// Массив данных для объявления
+// Создаем массив данных для объявления
 const USER_AVATARMIN = 1;
 const USER_AVATARMAX = 8;
 const OFFER_TITLE = [`Только для семьи котов`, `Работающим котам`, `Студентам из Котакадемии`];
@@ -20,11 +20,20 @@ const LOCATION_XMIN = 0;
 const LOCATION_XMAX = 1200;
 const LOCATION_YMIN = 130;
 const LOCATION_YMAX = 630;
+const PIN_WIDTH = 50;
+const PIN_HEIGHT = 70;
+const X = getRandomInteger(LOCATION_XMIN, LOCATION_XMAX);
+const Y = getRandomInteger(LOCATION_YMIN, LOCATION_YMAX);
 
 const similarListElement = document.querySelector(`.map__pins`);
 const similarUserTemplate = document.querySelector(`#pin`);
 
-// Функции для рандомного элемента из массива
+// Рассчитываем координаты для метки
+
+map__pin.style.left = X - PIN_WIDTH / 2;
+map__pin.style.top = Y - PIN_HEIGHT / 2;
+
+// Создаем функцию для рандомного элемента из массива
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -33,42 +42,42 @@ function getRandomArrayElement(array) {
   return array[getRandomInteger(0, array.length - 1)];
 }
 
-// Функция рандомной строки из массива (непонятно как вывести рандомное число элементов)
+// Создаем функцию рандомной строки из массива
 function getRandomArrayStroke(array) {
-  return array[Math.floor(Math.random() * array.length)];
+  let newArray = [];
+  for (let i = 1; i <= array.length; i++) {
+    let rnd = Math.floor(Math.random() * array.length);
+    newArray.push(array[rnd]);
+    array.splice(rnd, Math.floor(Math.random() * array.length));
+  }
 }
 
-// Функция для создания похожих объявлений
-const fragment = document.createDocumentFragment();
-
+// Создаем массив
+const dataArray = [];
 for (let i = 0; i < 8; i++) {
+  const offer = {
+    userAvatar: `img/avatars/user${getRandomInteger(USER_AVATARMIN, USER_AVATARMAX)}.png`,
+    offerTitle: getRandomArrayElement(OFFER_TITLE),
+    offerAddress: `${getRandomInteger(LOCATION_XMIN, LOCATION_XMAX)} ${getRandomInteger(LOCATION_YMIN, LOCATION_YMAX)}`,
+    offerPrice: getRandomArrayElement(OFFER_PRICE),
+    offerType: getRandomArrayElement(OFFER_TYPE),
+    offerRooms: getRandomArrayElement(OFFER_ROOMS),
+    offerGuests: getRandomArrayElement(OFFER_GUESTS),
+    offerCheckin: getRandomArrayElement(OFFER_CHECKIN),
+    offerCheckout: getRandomArrayElement(OFFER_CHECKOUT),
+    offerFeatures: getRandomArrayStroke(OFFER_FEATURES),
+    offerDescription: getRandomArrayElement(OFFER_DESCRIPTION),
+    offerPhotos: getRandomArrayStroke(OFFER_PHOTOS)
+  };
+  dataArray.push(offer);
+}
+
+const fragment = document.createDocumentFragment();
+for (let j = 0; j < dataArray.length; j++) {
   const userElement = similarUserTemplate.cloneNode(true);
-  const userAvatar = `img/avatars/user${getRandomInteger(USER_AVATARMIN, USER_AVATARMAX)}.png`;
-  const offerTitle = getRandomArrayElement(OFFER_TITLE);
-  const offerAddress = `${getRandomInteger(LOCATION_YMIN, LOCATION_YMAX)} ${getRandomInteger(LOCATION_XMIN, LOCATION_XMAX)}`;
-  const offerPrice = getRandomArrayElement(OFFER_PRICE);
-  const offerType = getRandomArrayElement(OFFER_TYPE);
-  const offerRooms = getRandomArrayElement(OFFER_ROOMS);
-  const offerGuests = getRandomArrayElement(OFFER_GUESTS);
-  const offerCheckin = getRandomArrayElement(OFFER_CHECKIN);
-  const offerCheckout = getRandomArrayElement(OFFER_CHECKOUT);
-  const offerFeatures = getRandomArrayStroke(OFFER_FEATURES);
-  const offerDescription = getRandomArrayElement(OFFER_DESCRIPTION);
-  const offerPhotos = getRandomArrayStroke(OFFER_PHOTOS);
-
-  userElement.querySelector(`#avatar`).src = userAvatar;
-  userElement.querySelector(`#title`).textContent = offerTitle;
-  userElement.querySelector(`#address`).textContent = offerAddress;
-  userElement.querySelector(`#price`).textContent = offerPrice;
-  userElement.querySelector(`#type`).textContent = offerType;
-  userElement.querySelector(`#room_number`).textContent = offerRooms;
-  userElement.querySelector(`#capacity`).textContent = offerGuests;
-  userElement.querySelector(`#timein`).textContent = offerCheckin;
-  userElement.querySelector(`#timeout`).textContent = offerCheckout;
-  userElement.querySelector(`.features`).textContent = offerFeatures;
-  userElement.querySelector(`#description`).textContent = offerDescription;
-  userElement.querySelector(`#images`).textContent = offerPhotos;
-
+  userElement.querySelector(`img`).src = offer.userAvatar;
+  userElement.querySelector(`img`).alt = offer.offerTitle;
   fragment.appendChild(userElement);
 }
+
 similarListElement.appendChild(fragment);
